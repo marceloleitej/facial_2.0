478bd5ba1c2dc859c93b68039196f58d50ccdbcf
marceloleitej <marcelo.teart@gmail.com>
2024-06-28 13:11:57 -0300

    open cam first file

---
+++ main.py
@@ -0,0 +1,14 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow('Face atempt', img)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
